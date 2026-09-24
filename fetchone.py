209dcd1303a399533c643af35d84a83f796f8deb
rajsingh65145-a11y <rajsingh65145@gmail.com>
2026-09-24 22:36:29 +0530

    hrstissji

--- fetchone.py
+++ fetchone.py
@@ -15,7 +15,7 @@ print("table successfully created")
 # step 3: insert multiple records      
 
 conn.execute("insert into tblemp11 values(1,'rohit','M','2001-12-21',24000)")
-conn.execute("insert into tblemp11 values(2,'karan','M','2007-9-11',50000)")
+conn.execute("insert into tblemp11 values(2,'karan','M','2007-9-11',50000)")  
 conn.execute("insert into tblemp11 values(3,'harsh','M','2006-3-11',60000)")
 conn.execute("insert into tblemp11 values(4,'laksh','M','2008-8-2',70000)")
 conn.commit()
